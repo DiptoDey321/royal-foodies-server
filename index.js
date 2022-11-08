ae2d974b3629d75fac16a7a43bcde5133cb98fd5
Dipto Dey <diptodey2005@gmail.com>
2022-11-09 00:03:18 +0600

    dotenv install and mongodb connection established

---
+++ index.js
@@ -0,0 +1,22 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+// middleware 
+app.use(cors())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1ybdqfv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+  
+  
+  app.listen(port, () => {
+    console.log(`Royal foodies server app listening on port ${port}`)
+  })
